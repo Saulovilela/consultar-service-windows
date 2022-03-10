3ea15d0f5e18fb94b521e5bd5fb4d6a781d3bb1e
Saulo Vilela <Saulovilelabarbosa@gmail.com>
2022-03-10 19:35:52 -0300

    comitado

---
+++ Sem título3.ps1
@@ -0,0 +1,10 @@
+﻿#Consultar serviço do windows
+$Serv= Get-Service -Name Spooler
+if ($Serv.Status -eq "Running") 
+  {
+  Write-Host "Em execução"
+  }
+  Else
+  {
+  Write-Host "Serviço Parado"
+  }
